6fd8fd4869a7167b6e71d94a833ba4a94aeb2ed9
mkuklik <mkuklik@gmail.com>
2024-11-04 00:57:06 -0800

    remove graph viewer

--- src/graphviewer/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import upload from './uploadReducer';
-
-export default combineReducers({
-  upload,
-});
--- src/graphviewer/selectors/uploadSelectors.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { path } from 'ramda';
-
-export const uploadObjectSelector = (state) => path(['upload', 'objects'], state);
-export const uploadFetchPendingSelector = (state) => path(['upload', 'fetchPending'], state);
-export const uploadHasPermissionSelector = (state) => path(['upload', 'hasPermission'], state);
